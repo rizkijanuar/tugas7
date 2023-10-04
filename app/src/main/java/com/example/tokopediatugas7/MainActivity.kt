49c993fa8cf0b7923477c12d35caf6bc22e504d4
Rizki Januar I <104696196+rzkjanuarr@users.noreply.github.com>
2023-10-04 18:00:43 +0700

    Menambahkan gambar, model, dan Adapter

--- app/src/main/java/com/example/tokopediatugas7/MainActivity.kt
+++ app/src/main/java/com/example/tokopediatugas7/MainActivity.kt
@@ -7,6 +7,9 @@ import com.example.tokopediatugas7.adapter.MainAdapter
 import com.example.tokopediatugas7.databinding.ActivityMainBinding
 import com.example.tokopediatugas7.model.Content1
 import com.example.tokopediatugas7.model.Content2
+import com.example.tokopediatugas7.model.Content3
+import com.example.tokopediatugas7.model.Content4
+import com.example.tokopediatugas7.model.Content5
 import com.example.tokopediatugas7.model.DataItem
 import com.example.tokopediatugas7.model.DataItemType
 
@@ -32,18 +35,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun allData() {
-        val content1List = ArrayList<Content1>()
-        content1List.add(Content1(R.drawable.icon1, "Coins"))
-        content1List.add(Content1(R.drawable.icon2, "Langganan" ))
-        content1List.add(Content1(R.drawable.icon3, "Gold"))
-        content1List.add(Content1(R.drawable.icon4, "More"))
+        val content1Data = ArrayList<Content1>()
+        content1Data.add(Content1(R.drawable.iv1, "Coins"))
+        content1Data.add(Content1(R.drawable.iv2, "Langganan" ))
+        content1Data.add(Content1(R.drawable.iv3, "Gold"))
+        content1Data.add(Content1(R.drawable.iv4, "More"))
 
-        val content2List = ArrayList<Content2>()
-        content2List.add(Content2(R.drawable.banner1))
-        content2List.add(Content2(R.drawable.banner2))
-        content2List.add(Content2(R.drawable.banner3))
+        val content2Data = ArrayList<Content2>()
+        content2Data.add(Content2(R.drawable.banner1))
+        content2Data.add(Content2(R.drawable.banner2))
+        content2Data.add(Content2(R.drawable.banner3))
 
-        listData.add(DataItem(DataItemType.CONTENT1, content1List, content2List))
-        listData.add(DataItem(DataItemType.CONTENT2, content1List, content2List))
+        val content3Data = ArrayList<Content3>()
+        content3Data.add(Content3(R.drawable.icon1, "Lihat Semua"))
+        content3Data.add(Content3(R.drawable.icon2, "Diskon 50%"))
+        content3Data.add(Content3(R.drawable.icon3, "Top-up "))
+        content3Data.add(Content3(R.drawable.icon4, "Travel "))
+        content3Data.add(Content3(R.drawable.icon5, "Promo Hari ini"))
+        content3Data.add(Content3(R.drawable.icon6, "Go Food"))
+
+        val content5Data = ArrayList<Content5>()
+        content5Data.add(Content5(R.drawable.top1, "Furniture"))
+        content5Data.add(Content5(R.drawable.top2, "Gadget"))
+        content5Data.add(Content5(R.drawable.top3, "Food"))
+        content5Data.add(Content5(R.drawable.top4, "Holiyay"))
+
+        listData.add(DataItem(DataItemType.CONTENT1, content1Data, content2Data, content3Data, content5Data))
+        listData.add(DataItem(DataItemType.CONTENT2, content1Data, content2Data, content3Data, content5Data))
+        listData.add(DataItem(DataItemType.CONTENT3, content1Data, content2Data, content3Data, content5Data))
+        listData.add(DataItem(DataItemType.CONTENT4, Content4("Lanjut disini bang")))
+        listData.add(DataItem(DataItemType.CONTENT5, content1Data, content2Data, content3Data, content5Data))
     }
 }
